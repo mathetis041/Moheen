2b49742ed640c290469be8ef01394f65553deb27
Michaelajayi150 <ajayimichael150@gmail.com>
2023-11-15 00:47:45 +0100

    Whitespace

--- src/components/products/index.jsx
+++ src/components/products/index.jsx
@@ -5,7 +5,7 @@ function ProductSection() {
   return (
     <section className="min-h-screen py-16 px-6 max-w-[1120px] mx-auto">
       <div className="text-center space-y-3">
-        <h2 className="text-5xl font-semibold">Products</h2>
+        <h2 className="text-5xl font-semibold">Products</h2>git
         <p className="text-lg text-gray-800">Get all you need from here</p>
       </div>
       <div className="flex gap-4 mt-10">
